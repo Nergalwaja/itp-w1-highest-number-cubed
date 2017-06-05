0859628abc8057f3b79d181c32fce863342b41fd
James Curbow <curbowj@uni.edu>
2017-06-05 01:25:36 +0000

    cubey project completed: 6/4/2017

--- highest_number_cubed/main.py
+++ highest_number_cubed/main.py
@@ -2,4 +2,17 @@
 
 
 def highest_number_cubed(limit):
-    pass
+    number = 0
+    while True :
+        number += 1
+        if number ** 3 > limit:
+            return number - 1
+highest_number_cubed(30)
+highest_number_cubed(12)
+highest_number_cubed(3)
+highest_number_cubed(12000)
+
+#1 ** 3 = 1
+#2 ** 3 = 8
+#3 ** 3 = 27
+#4 ** 3 = 64
